91916da0cd62275e868b37b932c243e63ee45c05
unknown <hshanto707@gmail.com>
2021-11-14 09:24:45 +0600

    service added

---
+++ src/Pages/ServicePage/ServicePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router';
+
+const ServicePage = () => {
+  const {name, description } = useParams();
+
+  return (
+    <div className="container mb-5" style={{marginTop:'200px'}}>
+      <h1 className="mb-4">{name}</h1>
+      <p style={{border: '2px solid gray', borderRadius: '10px', padding: '20px', textAlign: 'justify'}}>{description}</p>
+    </div>
+  );
+};
+
+export default ServicePage;
