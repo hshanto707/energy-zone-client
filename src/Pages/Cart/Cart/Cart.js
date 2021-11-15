d6bb5c708a90e784e5718f4b75da4331384ef90a
unknown <hshanto707@gmail.com>
2021-11-15 14:42:41 +0600

    cart added

---
+++ src/Pages/Cart/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = () => {
+  
+  
+  return (
+    <div className="cart-container" style={{margin:'200px 0'}}>
+      <h1>Cart</h1>
+    </div>
+  );
+};
+
+export default Cart;
